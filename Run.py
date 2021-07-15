ca01342c0a0d4d6f42c7a4b68bf6274a2c27f72d
ctt03012000 <85872476+ctt03012000@users.noreply.github.com>
2021-07-16 00:55:19 +0800

    Adjustments

--- Run.py
+++ Run.py
@@ -2,5 +2,5 @@ from AlphaEvolve import AlphaEvolve
 
 if __name__ == '__main__':
     ## frequency : '5min', '15min', '1H', '4H', '1D', '1W', '2W', '1M'
-    x = AlphaEvolve(name = 'abc', population = 50, tournament = 10, window = 30, numNewAlphaPerMutation = 50, trainRatio = 0.6, validRatio = 0.2, maxNumNodes = 1000, maxLenShapeNode = 50)
+    x = AlphaEvolve(name = 'abc', mutateProb = 0.3, population = 50, tournament = 10, window = 30, numNewAlphaPerMutation = 50, trainRatio = 0.7, validRatio = 0.29, maxNumNodes = 1000, maxLenShapeNode = 50)
     x.run()
